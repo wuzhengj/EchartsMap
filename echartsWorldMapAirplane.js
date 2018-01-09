4524b1445607f337b9b2d02e3d41e22abedff42a
WQMDC <244401893@qq.com>
2018-01-09 17:57:38 +0800

    增加图例，并且删除冗余代码

--- echartsWorldMapAirplane.js
+++ echartsWorldMapAirplane.js
@@ -175,6 +175,13 @@ define(["qlik", "./echarts/echarts", "./echarts/world"],
                 // 地图数据点开关
                 var DotOff = layout.map.props.DotOff;
 
+                // 图例内容
+                var lengendArr = qHyperCube.qMeasureInfo,
+                    lengendContent = [];
+                for (var z in lengendArr) {
+                    lengendContent.push(lengendArr[z].qFallbackTitle);
+                }
+
                 var planePath = 'path://M1705.06,1318.313v-89.254l-319.9-221.799l0.073-208.063c0.521-84.662-26.629-121.796-63.961-121.491c-37.332-0.305-64.482,36.829-63.961,121.491l0.073,208.063l-319.9,221.799v89.254l330.343-157.288l12.238,241.308l-134.449,92.931l0.531,42.034l175.125-42.917l175.125,42.917l0.531-42.034l-134.449-92.931l12.238-241.308L1705.06,1318.313z';
 
                 // 对航线点进行连接及经纬度坐标点匹配
@@ -274,7 +281,6 @@ define(["qlik", "./echarts/echarts", "./echarts/world"],
                             show: false
                         }
                     },
-                    // data: convertDataDot(dotData),
                     data: DotOff ? convertDataDot(dotData) : [],
                     tooltip: {
                         formatter: function(params) {
@@ -321,10 +327,20 @@ define(["qlik", "./echarts/echarts", "./echarts/world"],
                     }
                 };
 
+                var lengend = {
+                    type: 'plain',
+                    show: true,
+                    left: 'left',
+                    top: 'top',
+                    orient: 'horizontal',
+                    data: lengendContent
+                }
+
                 var option = {
                     tooltip: {
                         trigger: 'item'
                     },
+                    legend: lengend,
                     geo: geo,
                     visualMap: visualMap,
                     series: series
